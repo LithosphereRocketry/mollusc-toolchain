d1cca8dc683e36ee2003f6832f2e158a8e8646ad
bry <greatmaharg@gmail.com>
2025-07-18 00:52:20 -0400

    Assembling per section

--- targets/as/main.c
+++ targets/as/main.c
@@ -12,12 +12,19 @@ argument_t* args[] = {
     &arg_out
 };
 
-void assemble_section(void* global, const char* name, void* section) {
-    (void) global;
-    struct assembly_result res = assemble(section);
+void assemble_section(void* global, const char* name, void* value) {
+    struct string_map* bin_sections = global;
+    struct parse_section* section = value;
+    struct assembly_result* res = malloc(sizeof(struct assembly_result));
+    *res = assemble(section);
+    sm_put(bin_sections, name, res);
+}
+
+void print_destroy_asm_section(void* global, const char* name, void* res) {
+    (void) global, (void) name;
     printf("Assembly for section \"%s\":\n", name);
-    print_assembly(&res);
-    destroy_assembly(&res);
+    print_assembly(res);
+    destroy_assembly(res);
 }
 
 int main(int argc, char** argv) {
@@ -48,10 +55,11 @@ int main(int argc, char** argv) {
 
     struct parse_result parsed = asm_parse(filetext, filename);
     free(filetext);
-    printf("Parse result:\n");
-    print_parse(&parsed);
 
-    sm_foreach(&parsed.sections, assemble_section, NULL);
+    struct string_map bin_sections = sm_make();
+    sm_foreach(&parsed.sections, assemble_section, &bin_sections);
+    sm_foreach(&bin_sections, print_destroy_asm_section, NULL);
+    sm_destroy(&bin_sections, true);
     
     destroy_parse(&parsed);
 }
